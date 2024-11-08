8b08baf55736e1942647c2f141292efb6ad90a06
Emerson pinho <the.emerson.araujo@gmail.com>
2024-11-08 09:41:21 -0300

    adicionando mais um text(texto)

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,6 +2,9 @@ import { Text } from "react-native"
 
 export default function Index(){
     return(
+    <view>
         <Text>Hell react native!</Text>
+        <Text>Meu app</Text>
+    </view>
     )
 }
