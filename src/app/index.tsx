7237bd5255ea85d6c1a7fcda145d80b7c4f22226
Emerson pinho <the.emerson.araujo@gmail.com>
2024-11-09 17:30:24 -0300

    adicionando estilização

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,18 @@
-import { Text } from "react-native"
+import { Text, View, StyleSheet} from "react-native"
+import { red } from "react-native-reanimated/lib/typescript/reanimated2/Colors"
 
 export default function Index(){
     return(
     <view>
-        <Text>Hell react native!</Text>
-        <Text>Meu app</Text>
+        <Text style={{}}>Hell react native!</Text>
     </view>
     )
 }
+
+const style = StyleSheet.create({
+    title: {
+        color: "red",
+        fontSize: 22,
+    }
+
+})
