a5c6335361d40ba3be037863923ba8ea9270f774
Emerson pinho <the.emerson.araujo@gmail.com>
2024-11-09 21:59:27 -0300

    criando a estrutura da forma certa

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,5 @@
 import { Text, View, StyleSheet} from "react-native"
-import { red } from "react-native-reanimated/lib/typescript/reanimated2/Colors"
+
 
 export default function Index(){
     return(
