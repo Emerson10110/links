8c14f0b4cc2ba081f63606197528ec8df1ecd6bd
Emerson pinho <the.emerson.araujo@gmail.com>
2024-11-10 11:02:51 -0300

    difinindo a cor de fundo

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,15 @@
 import { Stack } from "expo-router"
 
+import { colors } from "@/styles/colors"
+
 export default function layout() {
-    return <Stack />
+    return (
+        <Stack 
+            screenOptions={{
+            headerShown: false,
+            contentStyle: { backgroundColor:
+            colors.gray[900] },
+        }}
+        />
+    )
 }
