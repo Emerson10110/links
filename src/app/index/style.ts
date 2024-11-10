ef33f4c70c50fd134a24450a75a4717ff06db3d2
Emerson pinho <the.emerson.araujo@gmail.com>
2024-11-10 17:18:57 -0300

    adicionando o estilo no index.tsx

--- src/app/index/style.ts
+++ src/app/index/style.ts
@@ -1,16 +1,21 @@
 import { StyleSheet } from "react-native"
 
 import { colors } from "@/styles/colors"
-import { Header } from "react-native/Libraries/NewAppScreen"
 
 export const style = StyleSheet.create({
     container: {
         flex: 1,
+        paddingTop: 62,
     },
     title: {
         color: colors.green[900],
         fontSize: 22,
     },
-    header: {},
-    logo: {},
+    header: {
+        paddingHorizontal: 24,
+     },
+    logo: {
+       height: 32,
+       width: 38, 
+    },
 })
