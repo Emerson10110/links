fa6acfd63ba7ea436ff2763173b8420b7d7c3833
Emerson pinho <the.emerson.araujo@gmail.com>
2024-11-11 19:21:11 -0300

    adicionei o category maiusculo

--- src/components/category/index.tsx
+++ src/components/category/index.tsx
@@ -6,7 +6,7 @@ import { Colors } from "react-native/Libraries/NewAppScreen"
 import { styles } from "./styles"
 
 
-export function category() {
+export function Category() {
     return (
         <Pressable style={styles.container}>
             <MaterialIcons name="code" size={16} color={Colors.gray[400]} />
